cb9dffb9aeec7d794637f6baad74c7d3014c05d3
FuryACE <sudhanshuvshekhar@gmail.com>
2022-11-16 01:57:21 +0530

    added carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,42 +1,84 @@
-import React from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import Image from 'next/image';
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-// import required modules
-import { EffectFade, Autoplay, Pagination, Navigation } from "swiper";
-
-export default function Carousel() {
-  return (
-    <div style={{ width: "50rem", height: "40rem" }}>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        effect={"fade"}
-        loop={true}
-        autoplay={{
-          delay: 5000,
-        }}
-        pagination={{
-          clickable: true
-        }}
-        // navigation={true}
-        modules={[EffectFade, Autoplay, Pagination, Navigation]}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </div >
-  );
+import swarajImg from '../../../public/solar-bus.jpeg';
+export default class Carousel extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      fade: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 8000,
+      speed: 500,
+      lazyLoad: true,
+      slidesToShow: 1,
+      arrows: true,
+      slidesToScroll: 1
+    };
+    return (
+      <>
+        <Slider {...settings}>
+          <div>
+            <Image
+              src={swarajImg}
+              alt="solar panels"
+              width="1880rem"
+              height="750rem"
+              style={{ borderRadius: '30px' }}
+            />
+          </div>
+          <div>
+            <Image
+              src="https://images.unsplash.com/photo-1497440001374-f26997328c1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+              alt="solar panels"
+              width="1880rem"
+              height="750rem"
+              style={{ borderRadius: '30px' }}
+            />
+          </div>
+          <div>
+            <Image
+              src="https://images.unsplash.com/photo-1532601224476-15c79f2f7a51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="solar panels"
+              width="1880rem"
+              height="750rem"
+              style={{ borderRadius: '30px' }}
+            />
+          </div>
+          <div>
+            <Image
+              src="https://images.unsplash.com/photo-1472313420546-a46e561861d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+              alt="solar panels"
+              width="1880rem"
+              height="750rem"
+              style={{ borderRadius: '30px' }}
+            />
+          </div>
+          <div>
+            <Image
+              src="https://images.unsplash.com/photo-1473341304170-971dccb5ac1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="solar panels"
+              width="1880rem"
+              height="750rem"
+              style={{ borderRadius: '30px' }}
+            />
+          </div>
+          <div>
+            <Image
+              src="https://images.unsplash.com/photo-1473876637954-4b493d59fd97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1092&q=80"
+              alt="solar panels"
+              width="1880rem"
+              height="750rem"
+              style={{ borderRadius: '30px' }}
+            />
+          </div>
+        </Slider>
+      </>
+    );
+  }
 }
